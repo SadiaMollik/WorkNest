b1d83d0c1f22023f5284dbc592da0337a90c03c6
noxustic <saadat20022002@gmail.com>
2025-12-16 14:16:38 +0600

    updated

--- worknest-client-side/src/contexts/NotificationContext.jsx
+++ worknest-client-side/src/contexts/NotificationContext.jsx
@@ -9,9 +9,7 @@ const NotificationContext = createContext();
 export const useNotification = () => {
   const context = useContext(NotificationContext);
   if (!context) {
-    throw new Error(
-      "useNotification must be used within a NotificationProvider"
-    );
+    throw new Error("useNotification must be used within a NotificationProvider");
   }
   return context;
 };
